af07129f2d6447fd2c914306b0ccf47509fc5baa
unknown <mical.juarez@gmail.com>
2018-08-15 23:59:07 -0500

    playing with material ui

--- react-example/app/app.js
+++ react-example/app/app.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hello, world</h1>
+        <h1>React Todo App</h1>
       </div>
     );
   }
--- react-example/webpack.config.js
+++ react-example/webpack.config.js
@@ -19,3 +19,28 @@ module.exports = {
     }]
   }
 }
+
+module.exports = [{
+  entry: './app/css/app.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'bundle.css',
+          },
+        },
+        { loader: 'extract-loader' },
+        { loader: 'css-loader' },
+        { loader: 'sass-loader' },
+      ]
+    }]
+  },
+}];
